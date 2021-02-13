715d8362eacb41a5f8404d3c9bb1a1577c82b9a0
kyle <hollarskyle@gmail.com>
2021-02-13 12:02:43 -0500

    Commented and streamlined, mostly in HardwareAndMethods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareAndMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareAndMethods.java
@@ -27,46 +27,64 @@ import java.util.List;
 
 //@Disabled
 public class HardwareAndMethods {
-    //CV stuff copy/pasted from ConceptTensorFlowObjectDetectionEngine
-    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite"; //TODO: Replace with each year's new file & names
-    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    //Variables Necessary for Computer Vision, they will be different each year
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad"; //
     private static final String LABEL_SECOND_ELEMENT = "Single";
-    private static final String VUFORIA_KEY =
-            "ATwu5Uf/////AAABmW6EkCcqK08LuKL127zX3owl9yKuVGftG+fZJh1x5DAia5zv6SAip+KJqd+P9DUA1NhLaqowDt2iraVhn4mt8C2ZJXuQd5XZiQ9Ihx6s1dvI0W+/bn0YBP2rry4keEQC2C2NVmfAtXsV+sqYVGfkHUkQ1n00L/ndbQKPE0JMWFo+sX5683ght4wyaTjjKbjSxL8gNVoCaP9ndedm+tsPdfKcSj8urqhgSOtNlAk4cTzVx1buSG33tNKy4a+JuULWYFbRngrEqPd/6MzIXFAxpMvfu0dcpCFal+VYPX/upaL4GkEEV2gfJ4xtSEqIBgnIFM8WtWwC51P2yhtGjDoqXChw0MvLDjcCTxqYT3MIjFE1";
+    private static final String VUFORIA_KEY = Secret.vKey; /*you'll have to get your own developer key,
+     It should look like the one below, store it as a String */
+    //"ATwu5Uf/////AAABmW6EkCcqK08LuKL127zX3owl9yKuVGftG+fZJh1x5DAia5zv6SAip+KJqd+P9DUA1NhLaqowDt2iraVhn4mt8C2ZJXuQd5XZiQ9Ihx6s1dvI0W+/bn0YBP2rry4keEQC2C2NVmfAtXsV+sqYVGfkHUkQ1n00L/ndbQKPE0JMWFo+sX5683ght4wyaTjjKbjSxL8gNVoCaP9ndedm+tsPdfKcSj8urqhgSOtNlAk4cTzVx1buSG33tNKy4a+JuULWYFbRngrEqPd/6MzIXFAxpMvfu0dcpCFal+VYPX/upaL4GkEEV2gfJ4xtSEqIBgnIFM8WtWwC51P2yhtGjDoqXChw0MvLDjcCTxqYT3MIjFE1";
 
     //vuforia is the variable we will use to store our instance of the Vuforia localization engine
     private VuforiaLocalizer vuforia;
-
     //tfod is the variable we will use to store our instance of the TensorFlow Object Detection Engine
     public TFObjectDetector tfod;
 
-    //Declares variables
-    //Motors, just the wheels
-    //DcMotor motorName = null;
+/*******************************************************************************************/
+    /*
+    * The following are variables that let you interface with the hardware of the robot
+    * you can change this as you need, but this will work for a very simple robot with:
+    * -4 Mecanum wheels
+    * -1 Servo
+    * -One imu sensor
+    * -A forward and sideways encoder (see wiki)
+    * -A distance sensor
+    * -A color sensor
+    * You can copy/paste, comment-out, or delete any of these to create new or get rid of unused
+    * sensors or motors.
+    *
+    * Keep in mind that you have to map all of these to actual hardware by naming the hardware
+    * on the phone app.
+    */
+
+    //Motors | DcMotor motorName = null;
     DcMotor leftFront = null;
     DcMotor leftBack = null;
     DcMotor rightFront = null;
     DcMotor rightBack = null;
-    //The speed at which the motors will turn
-    public float speedMod = 1f;
+    public float speedMod = 1f; //The speed multiplier for the motors
 
-    //Servos
-    //Servo servoName = null;
+    //Servos | Servo servoName = null;
+    Servo servo1 = null;
 
     //Sensors
     DcMotor forwardEncoder = null;
     DcMotor sidewaysEncoder = null;
-    BNO055IMU imu; // https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/overview ??
-    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters(); //leaving uncommented because I'm not sure why it's here
+
+    BNO055IMU imu; //https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/overview
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
     DistanceSensor distance = null;
-    //ColorSensor colorSensorName = null;
-    //float hsvValuesSensor[] = {0F, 0F, 0F}; //Color sensor Hue Saturation Value;; one for each ColorSensor
-    //final double COLOR_SCALE_FACTOR = 255;
 
-  HardwareMap hwMap = null;
+    ColorSensor colorSensor = null;
+    float hsvValuesSensor[] = {0F, 0F, 0F}; //Color sensor Hue Saturation Value;; one for each ColorSensor
+    final double COLOR_SCALE_FACTOR = 255;
 
+/**************************************************************************************************/
+
+    HardwareMap hwMap = null;
     private ElapsedTime runtime = new ElapsedTime();
-    private boolean firstMove = true;
+
     public HardwareAndMethods(){
         //Set up parameters for imu
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
@@ -74,28 +92,25 @@ public class HardwareAndMethods {
         parameters.calibrationDataFile = "BNO055IMUCalibration.json";
         parameters.loggingEnabled      = true;
         parameters.loggingTag          = "IMU";
-//        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        //parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
     }
     public void init(HardwareMap ahwMap) {
-//        hwMap = ahwMap;
+        hwMap = ahwMap;
 
-        // Initialize the hardware variables
-        //name = hwMap.get(Type.class "deviceName");
+        // Initialize the hardware variables | name = hwMap.get(Type.class "deviceName");
 
-        //Motors; declared above;
+        //Motors; declared above; TODO:"Initialize variables here?"
         leftFront  = hwMap.get(DcMotor.class, "leftFront");
         leftBack = hwMap.get(DcMotor.class, "leftBack");
         rightFront = hwMap.get(DcMotor.class, "rightFront");
         rightBack = hwMap.get(DcMotor.class, "rightBack");
 
-
         //Servos
-        //exampleName = hwMap.get(Servo.class, "exampleName");
-
+        servo1 = hwMap.get(Servo.class, "servo1");
 
         //Sensors
         imu = hwMap.get(BNO055IMU.class, "imu");
-        //colorSensor = hwMap.get(ColorSensor.class, "colorSensor");
+        colorSensor = hwMap.get(ColorSensor.class, "colorSensor");
         distance = hwMap.get(DistanceSensor.class, "distance");
         forwardEncoder = hwMap.get(DcMotor.class, "forward");
         sidewaysEncoder = hwMap.get(DcMotor.class, "sideways");
@@ -122,6 +137,7 @@ public class HardwareAndMethods {
 
 
     public void mecanum(float x, float y, float r){
+        //TODO: Create wiki page for mecanum
         double leftFrontPower = Range.clip(y + x + r, -1.0, 1.0) / speedMod;
         double leftBackPower = Range.clip(y - x + r, -1.0, 1.0) / speedMod;
         double rightFrontPower = Range.clip(y - x - r, -1.0, 1.0) / speedMod;
@@ -135,6 +151,11 @@ public class HardwareAndMethods {
     }
 
     public float map(float input, float inputMin, float inputMax, float outputMin, float outputMax){
+        /*
+        * Simple method to translate a number from one range to another
+        * Useful for situations using an analog input (perhaps a joystick or trigger) that returns
+        * a value between -1 and 1, and translating that into movement on a larger scale.
+        */
         return (input - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin;
     }
 
@@ -143,37 +164,50 @@ public class HardwareAndMethods {
         return orientation.firstAngle;
     }
 
-    public String getOdometry(){ //for telemetry
+    public String getOdometry(){
+        /*
+        For telemetry purposes, returns a String "x: ___, y: ___"
+        where the value after "x: " is the distance that the sideways encoder has travelled, and
+        the value after "y: " is the distance that the forward encoder has travelled.
+        */
         return "x: " + sidewaysEncoder.getCurrentPosition() + ", y: " + forwardEncoder.getCurrentPosition();
     }
 
     public boolean wheelsAreBusy(){
+        /*
+        * Returns true if any of the wheels are spinning
+        */
         return leftFront.isBusy() || leftBack.isBusy() || rightFront.isBusy() || rightBack.isBusy();
     }
 
 
-    //CV Stuff, maybe move to another class?
+    //TODO:CV Stuff, maybe move to another class?
     public void initCompuVis() {
-        //initVuforia()
-        VuforiaLocalizer.Parameters vuforiaParameters = new VuforiaLocalizer.Parameters();
+        /*
+        *  A combination of the initVuforia() and initTfod() methods from the Tensorflow tutorial
+        *  TODO: include link
+        *  TODO: verify that it... works
+        */
 
+        /* ** initVuforia() ** */
+        VuforiaLocalizer.Parameters vuforiaParameters = new VuforiaLocalizer.Parameters();
         vuforiaParameters.vuforiaLicenseKey = VUFORIA_KEY;
         vuforiaParameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        //Change cameraDirection if you use the 'selfie' camera
+        vuforia = ClassFactory.getInstance().createVuforia(vuforiaParameters); //Instantiate the Vuforia engine
+        /* **************** */
 
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(vuforiaParameters);
-
-        //initTfod()
+        /* ** initTfod() ** */
         int tfodMonitorViewId = hwMap.appContext.getResources().getIdentifier(
                 "tfodMonitorViewId", "id", hwMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
         tfodParameters.minResultConfidence = 0.8f;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
-
+        /* **************** */
     }
 
-    public void runTfod(){
+    public void runTfod(){ //TODO: Return List<Recognition>
         List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
         if (updatedRecognitions != null) {
 
